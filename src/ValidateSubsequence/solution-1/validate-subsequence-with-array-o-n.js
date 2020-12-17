d0b3fb32ca523af12ed2fdd4425fc76c3d92fc39
Marcell <marcell.alves@gmail.com>
2020-12-17 14:38:35 -0300

    using array iterator

--- src/ValidateSubsequence/solution-1/validate-subsequence-with-array-o-n.js
+++ src/ValidateSubsequence/solution-1/validate-subsequence-with-array-o-n.js
@@ -1,8 +1,8 @@
 module.exports = (array, sequence) => {
   let sequenceIndex = 0
 
-  for (let mainIndex = 0; mainIndex < array.length; mainIndex += 1) {
-    if (array[mainIndex] === sequence[sequenceIndex]) {
+  for (let element of array) {
+    if (element === sequence[sequenceIndex]) {
       sequenceIndex += 1
     }
 
