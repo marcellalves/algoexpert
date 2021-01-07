8528ee8c60d303500bd17b78d267742373266868
Marcell <marcell.alves@gmail.com>
2021-01-07 00:32:07 -0300

    primeira implementacao correta com testes passando e aceito pela plataforma do AlgoExpert

--- src/Algorithms/NodeDepths/solution-1/node-depths.js
+++ src/Algorithms/NodeDepths/solution-1/node-depths.js
@@ -1,3 +1,16 @@
 module.exports = (tree) => {
-  return 16
+  const depths = []
+  calculateSumNodeDepths(tree, 0, depths)
+  return depths.reduce((a, b) => { return a + b })
+}
+
+function calculateSumNodeDepths(node, depth, depths) {
+  if (node === null) {
+    return 
+  }
+
+  depths.push(depth)
+
+  calculateSumNodeDepths(node.right, depth + 1, depths)
+  calculateSumNodeDepths(node.left, depth + 1, depths)
 }
