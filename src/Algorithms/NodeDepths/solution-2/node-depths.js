a676d643efee6e2cf41ee1774f16bc2fb7a6e20e
Marcell <marcell.alves@gmail.com>
2021-01-07 09:02:36 -0300

    simplificando solucao 2

--- src/Algorithms/NodeDepths/solution-2/node-depths.js
+++ src/Algorithms/NodeDepths/solution-2/node-depths.js
@@ -1,12 +1,10 @@
 // O(n) time | O(h) space complexit, where h is the height of the tree
-module.exports = (tree) => {
-  return calculateNodeDepths(tree, 0)
-}
-
-function calculateNodeDepths(node, depth) {
+function calculateNodeDepths (node, depth = 0) {
   if (node === null) {
     return 0
   }
 
   return depth + calculateNodeDepths(node.left, depth + 1) + calculateNodeDepths(node.right, depth + 1)
-} 
+}
+
+module.exports = calculateNodeDepths
