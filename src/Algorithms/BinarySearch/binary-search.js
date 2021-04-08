fe2b62411d79acfc9c8a91e4d12a5de7ab04931e
Marcell <marcell.alves@gmail.com>
2021-04-08 13:04:35 -0300

    Solving binary search in an sorted array

--- src/Algorithms/BinarySearch/binary-search.js
+++ src/Algorithms/BinarySearch/binary-search.js
@@ -1,22 +1,29 @@
 function binarySearchArray (array, target) {
-  if (array.length === 0) {
-    return -1
+  let begin = 0
+  let end = array.length - 1
+  let pivot = 1
+
+  while (begin <= end) {
+    if (begin < end) {
+      pivot = Math.floor((end + begin) / 2)
+    } else {
+      pivot = begin
+    }
+
+    if (array[pivot] === target) {
+      return pivot
+    }
+
+    if (array[pivot] > target) {
+      end = pivot - 1
+    }
+
+    if (array[pivot] < target) {
+      begin = pivot + 1
+    }
   }
 
-  const pivot = ~~(array.length / 2)
-  const value = array[pivot]
-
-  if (target === value) {
-    return pivot
-  }
-
-  if (target < value) {
-    return binarySearchArray(array.slice(0, pivot), target)
-  }
-
-  if (target > value) {
-    return binarySearchArray(array.slice(pivot + 1))
-  }
+  return -1
 }
 
 module.exports = binarySearchArray
